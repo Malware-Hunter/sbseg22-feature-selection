565dccfd5d612b9fe6fe0fa447510d9f5c92fa2a
Diego Kreutz <diego.unipampa@gmail.com>
2022-05-27 16:19:26 -0300

    last updates done at Fri May 27 16:19:26 -03 2022

--- roda_jowmdroid.sh
+++ roda_jowmdroid.sh
@@ -3,6 +3,6 @@
 for DATASET in datasets/*.csv
 do
     echo -n "python3 methods/JOWMDroid/JOWMDroid.py -d $DATASET ... "
-    python3 methods/JOWMDroid/JOWMDroid.py -d $DATASET
+    python3 methods/JOWMDroid/JOWMDroid.py --feature-selection-only -d $DATASET
     echo "done."
 done
