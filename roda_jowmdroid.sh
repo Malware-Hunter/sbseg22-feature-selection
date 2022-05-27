54ae93612f40f0121671eee5a2405383c62e427a
Diego Kreutz <diego.unipampa@gmail.com>
2022-05-27 20:58:41 -0300

    last updates done at sex 27 mai 2022 20:58:41 -03

--- roda_jowmdroid.sh
+++ roda_jowmdroid.sh
@@ -1,8 +1,9 @@
+CHECK_PIP=$(which pip)
+[ "$CHECK_PIP" != "" ] || { echo "instale o pip: sudo apt -y install python3-pip"; exit; }
 #sudo pip install numpy scikit-learn scipy pandas
 
 for DATASET in datasets/*.csv
 do
-    echo -n "python3 methods/JOWMDroid/JOWMDroid.py -d $DATASET ... "
+    echo "python3 methods/JOWMDroid/JOWMDroid.py --feature-selection-only -d $DATASET ... "
     python3 methods/JOWMDroid/JOWMDroid.py --feature-selection-only -d $DATASET
-    echo "done."
 done
