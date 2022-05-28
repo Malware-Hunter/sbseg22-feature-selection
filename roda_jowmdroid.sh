a4f12786702165d1f68e56b604d32d427e8d1ae3
Diego Kreutz <diego.unipampa@gmail.com>
2022-05-27 21:04:33 -0300

    last updates done at sex 27 mai 2022 21:04:33 -03

--- roda_jowmdroid.sh
+++ roda_jowmdroid.sh
@@ -1,6 +1,7 @@
 CHECK_PIP=$(which pip)
 [ "$CHECK_PIP" != "" ] || { echo "instale o pip: sudo apt -y install python3-pip"; exit; }
-#sudo pip install numpy scikit-learn scipy pandas
+CHECK_PKGS=$(pip show numpy scipy pandas scikit-learn | grep -i -w "not found")
+[ "$CHECK_PKGS" != "" ] || { echo "instale os pacotes Python: sudo pip install numpy scikit-learn scipy pandas"; exit; }
 
 for DATASET in datasets/*.csv
 do
