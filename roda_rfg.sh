30d2bacab954012b3fa67e96d62d3ca0b9aa74a6
estevaog3 <eccosta@protonmail.com>
2022-05-27 20:10:43 -0400

    Add roda_rfg.sh script

---
+++ roda_rfg.sh
@@ -0,0 +1,29 @@
+echo "Preparando ambiente virtual ..."
+
+VENV="venv-rfg"
+PYTHON="${VENV}/bin/python3"
+PIP="${VENV}/bin/pip3"
+BASE_DIR="methods/RFG"
+CANT_INSTALL_VENV_MESSAGE="ERRO: não foi possível criar o ambiente virtual (venv).\nO python3.8 e o python3.8-venv estão instalados? Se não, use o comando: sudo apt install python3.8 python3.8-venv"
+
+if ! [[ -d "$VENV" ]]; then
+  python3 -m venv "$VENV"
+  ! [[ -d "$VENV" ]] && echo -e "$CANT_INSTALL_VENV_MESSAGE" && exit 1
+fi
+$PIP install numpy==1.22.3 wheel
+$PIP install -r $BASE_DIR/requirements.txt
+
+echo "Ambiente virtual preparado em \"${VENV}\""
+if [[ `ls -1 datasets/*.csv 2>/dev/null | wc -l ` -eq 0 ]]; then
+  echo "ERRO: não foi possível encontrar arquivos CSV no diretório \"datasets\"."
+  exit 1
+fi
+for DATASET in datasets/*.csv
+do
+    DATASET_SHAPE="`wc -l < "$DATASET"`,`head -1 "$DATASET" | awk -F, '{print NF}'`"
+    echo -n "qtd de features para o dataset $DATASET (linhas,colunas: $DATASET_SHAPE): "
+    read FEATURES_LIST
+    echo "$PYTHON $BASE_DIR/rfg.py -d $DATASET -f $FEATURES_LIST --feature-selection-only"
+    $PYTHON $BASE_DIR/rfg.py -d $DATASET -f $FEATURES_LIST --feature-selection-only
+    echo "Done"
+done
