e5e8e3bed881aade25ed69598bc37f276cb3b26e
estevaog3 <eccosta@protonmail.com>
2022-05-31 09:21:15 -0400

    Update roda_jowmdroid.sh: exit if no dataset is found

--- roda_jowmdroid.sh
+++ roda_jowmdroid.sh
@@ -3,6 +3,10 @@ CHECK_PIP=$(which pip)
 CHECK_PKGS=$(pip show numpy scipy pandas scikit-learn | grep -i -w "not found")
 [ "$CHECK_PKGS" = "" ] || { echo "instale os pacotes Python: sudo pip install numpy scikit-learn scipy pandas"; exit; }
 
+if [[ `ls -1 datasets/*.csv 2>/dev/null | wc -l ` -eq 0 ]]; then
+  echo "ERRO: não foi possível encontrar arquivos CSV no diretório \"datasets\"."
+  exit 1
+fi
 for DATASET in datasets/*.csv
 do
     D_NAME=$(echo $DATASET | cut -d"/" -f2)
