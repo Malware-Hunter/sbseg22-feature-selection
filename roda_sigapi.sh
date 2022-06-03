728ee35eb458f8bf4e892b6e12e9b5096944c52a
estevaog3 <eccosta@protonmail.com>
2022-06-03 14:15:40 -0400

    Add roda_sigapi.sh

---
+++ roda_sigapi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CHECK_PIP=$(which pip)
+[ "$CHECK_PIP" != "" ] || { echo "instale o pip: sudo apt -y install python3-pip"; exit; }
+PKGS=(seaborn pandas numpy scikit-learn)
+CHECK_PKGS=`pip show ${PKGS[@]} | grep -i -w "not found"`
+[ "$CHECK_PKGS" = "" ] || { echo "instale os pacotes Python: sudo pip install ${PKGS[@]}"; exit; }
+
+for DATASET in datasets/*.csv
+do
+    echo "python3 -m methods.SigAPI.sigapi_funcoesdeselecao -d $DATASET"
+    python3 -m methods.SigAPI.sigapi_funcoesdeselecao -d $DATASET
+    # TODO: passar o valor correto de num_features e method na linha a seguir:
+    python3 -m methods.SigAPI.sigapi_correlacao -d $DATASET -k num_features -m method
+done
