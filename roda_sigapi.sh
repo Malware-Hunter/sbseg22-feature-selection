e5af1b493acec969878a64c0fa9462f4b121f5ab
estevaog3 <eccosta@protonmail.com>
2022-06-06 17:01:29 -0400

    Update roda_sigapi.sh: set output file option

--- roda_sigapi.sh
+++ roda_sigapi.sh
@@ -9,8 +9,9 @@ bash setup_datasets.sh
 [[ $? != 0 ]] && exit 1
 for DATASET in datasets/*.csv
 do
-    echo "python3 -m methods.SigAPI.sigapi_funcoesdeselecao -d $DATASET"
-    OUTPUT=`python3 -m methods.SigAPI.sigapi_funcoesdeselecao -d $DATASET`
+    D_NAME=$(echo $DATASET | cut -d"/" -f2)
+    echo "python3 -m methods.SigAPI.sigapi_funcoesdeselecao -d $DATASET -o resultado-selecao-$D_NAME"
+    OUTPUT=`python3 -m methods.SigAPI.sigapi_funcoesdeselecao -d $DATASET -o resultado-selecao-$D_NAME`
     if [[ `echo $OUTPUT | grep -o 'Menor limite inferior encontrado:'` = "" ]]; then
         echo -n "Informe o método para a etapa de correlação 
         (mutualInformation, selectRandom, selectExtra, RFERandom, RFEGradient, selectKBest): "
@@ -21,6 +22,6 @@ do
         METHOD=`tail -1 | sed 's/ //g' | cut -d, -f1`
         NUM_FEATURES=`tail -1 | sed 's/ //g' | cut -d, -f2`
     fi
-    echo "python3 -m methods.SigAPI.sigapi_correlacao -d $DATASET -k $NUM_FEATURES -m $METHOD"
-    python3 -m methods.SigAPI.sigapi_correlacao -d $DATASET -k $NUM_FEATURES -m $METHOD
+    echo "python3 -m methods.SigAPI.sigapi_correlacao -d $DATASET -k $NUM_FEATURES -m $METHOD -o resultado-correlacao-$D_NAME"
+    python3 -m methods.SigAPI.sigapi_correlacao -d $DATASET -k $NUM_FEATURES -m $METHOD -o resultado-correlacao-$D_NAME
 done
