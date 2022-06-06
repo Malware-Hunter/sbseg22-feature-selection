82956ba510d98e916060ed80e0b81244cfa568d4
estevaog3 <eccosta@protonmail.com>
2022-06-06 13:10:12 -0400

    Update setup_datasets.sh: handle .rar datasets

--- setup_datasets.sh
+++ setup_datasets.sh
@@ -6,6 +6,11 @@ do
     unzip -n $DATASET -d datasets
 done
 
+for DATASET in datasets/*.rar
+do
+    unrar -o- $DATASET -d datasets
+done
+
 if [[ `ls -1 datasets/*.csv 2>/dev/null | wc -l ` -eq 0 ]]; then
   echo "ERRO: não foi possível encontrar arquivos CSV no diretório \"datasets\"."
   exit 1
