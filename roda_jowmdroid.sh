daf50b6d7c8e03d9e74bedaf9bdb9c31f88aae8f
Diego Kreutz <diego.unipampa@gmail.com>
2022-06-06 10:24:38 -0300

    [COMMIT-TS:20220606102438] roda_jowmdroid.sh adaptado para datasets compactados .rar

--- roda_jowmdroid.sh
+++ roda_jowmdroid.sh
@@ -7,9 +7,23 @@ if [[ `ls -1 datasets/*.csv 2>/dev/null | wc -l ` -eq 0 ]]; then
   echo "ERRO: não foi possível encontrar arquivos CSV no diretório \"datasets\"."
   exit 1
 fi
-for DATASET in datasets/*.csv
-do
+
+roda_dataset() {
+    DATASET=$1
     D_NAME=$(echo $DATASET | cut -d"/" -f2)
     echo "python3 methods/JOWMDroid/JOWMDroid.py --feature-selection-only -d $DATASET --output-file jowmdroid-$D_NAME... "
     python3 methods/JOWMDroid/JOWMDroid.py --feature-selection-only -d $DATASET --output-file jowmdroid-$D_NAME
+}
+
+for DATASET in datasets/*.csv
+do
+    roda_dataset $DATASET
 done
+
+for DATASET in datasets/*.rar
+do
+    unrar $DATASET
+    DATASET=$(echo $DATASET | sed 's/.rar/.csv/')
+    roda_dataset $DATASET
+done
+
