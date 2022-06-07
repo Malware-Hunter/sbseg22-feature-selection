d92932de48714dae7d94e7831436ec417a2e2f93
Luiza Paula Moreira Leão <95256271+luizaleao@users.noreply.github.com>
2022-06-07 15:45:18 -0300

    import matplotlib.pyplot

--- methods/SigAPI/sigapi_funcoesdeselecao.py
+++ methods/SigAPI/sigapi_funcoesdeselecao.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.ensemble import ExtraTreesClassifier
