f2de86b28b0b27f0567d2a3ea02e7e2a7b3535e0
estevaog3 <eccosta@protonmail.com>
2022-06-09 15:25:51 -0400

    Update roda_rfg.sh: save execution time to file

--- roda_rfg.sh
+++ roda_rfg.sh
@@ -26,11 +26,13 @@ bash setup_datasets.sh
 [[ $? != 0 ]] && exit 1
 
 MAX_N_FEATURES=100
+
 for DATASET in datasets/*.csv
 do
     TOTAL_N_FEATURES=`head -1 "$DATASET" | awk -F, '{print NF}'`
     [[ $TOTAL_N_FEATURES -gt $MAX_N_FEATURES ]] && N_FEATURES=$MAX_N_FEATURES || N_FEATURES=`expr $TOTAL_N_FEATURES - 1`
-    echo "$PYTHON $BASE_DIR/rfg.py -d $DATASET -f $N_FEATURES --feature-selection-only"
-    $PYTHON -m methods.RFG.rfg -d $DATASET -f $N_FEATURES --feature-selection-only
+    D_NAME=$(echo $DATASET | cut -d"/" -f2)
+    echo "$PYTHON -m methods.RFG.rfg -d $DATASET -f $N_FEATURES --feature-selection-only -o $D_NAME"
+    { time $PYTHON -m methods.RFG.rfg -d $DATASET -f $N_FEATURES --feature-selection-only -o $D_NAME; } 2> time-rfg-top-$N_FEATURES-$D_NAME.txt
     echo "Done"
 done
