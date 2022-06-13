40be194c69a59efde26a48cee13f89a1c7cc68cb
estevaog3 <eccosta@protonmail.com>
2022-06-12 21:46:12 -0400

    Update roda_sigapi.sh: get lower bound; pass in $D_NAME as argument

--- roda_sigapi.sh
+++ roda_sigapi.sh
@@ -18,16 +18,10 @@ sigapi(){
     set_increment `head -1 $DATASET | awk -F, '{print NF-1}'`
     echo "python3 -m methods.SigAPI.sigapi_funcoesdeselecao -d $DATASET -o resultado-selecao-$D_NAME -i $INCREMENT"
     OUTPUT=`python3 -m methods.SigAPI.sigapi_funcoesdeselecao -d $DATASET -o resultado-selecao-$D_NAME -i $INCREMENT`
-    if [[ `echo $OUTPUT | grep -o 'Menor limite inferior encontrado:'` = "" ]]; then
-        echo -n "Informe o método para a etapa de correlação 
-        (mutualInformation, selectRandom, selectExtra, RFERandom, RFEGradient, selectKBest): "
-        read METHOD
-        echo -n "Informe o limite inferior do intervalo mínimo: "
-        NUM_FEATURES
-    else
-        METHOD=`echo $OUTPUT | tail -1 | sed 's/ //g' | cut -d, -f1`
-        NUM_FEATURES=`echo $OUTPUT | tail -1 | sed 's/ //g' | cut -d, -f2`
-    fi
+    echo $OUTPUT
+    LOWER_BOUND=`echo $OUTPUT | grep -o -A 1 'Menor limite inferior encontrado:'`
+    METHOD=`echo $LOWER_BOUND | tail -1 | sed 's/ //g' | cut -d, -f1`
+    NUM_FEATURES=`echo $LOWER_BOUND | tail -1 | sed 's/ //g' | cut -d, -f2`
     echo "python3 -m methods.SigAPI.sigapi_correlacao -d $DATASET -k $NUM_FEATURES -m $METHOD -o resultado-correlacao-$D_NAME"
     python3 -m methods.SigAPI.sigapi_correlacao -d $DATASET -k $NUM_FEATURES -m $METHOD -o resultado-correlacao-$D_NAME
 }
@@ -39,5 +33,5 @@ for DATASET in $*
 do
     D_NAME=$(echo $DATASET | cut -d"/" -f2)
     TS=$(date +%Y%m%d%H%M%S)
-    { time sigapi $DATASET; } 2> time-sigapi-$D_NAME-$TS.txt
+    { time sigapi $DATASET $D_NAME; } 2> time-sigapi-$D_NAME-$TS.txt
 done
