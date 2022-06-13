ca5e855c246660342fdf132d0204e1a198602340
estevaog3 <eccosta@protonmail.com>
2022-06-12 23:14:03 -0400

    Update SigAPI code: remove outdated files; define correcacao in main.py

--- methods/SigAPI/main.py
+++ methods/SigAPI/main.py
@@ -16,7 +16,33 @@ import sys
 from random import choice
 from argparse import ArgumentParser
 from methods.utils import get_base_parser, get_dataset, get_X_y
-from methods.SigAPI.sigapi_correlacao import correlacao
+
+def correlacao(X, y, k, method, output_file, methods):
+    feature_scores = methods[method]['function'](X, y, k)
+    new_X = X[list(feature_scores['features'])]
+    
+    correlation = new_X.corr()
+    
+    model_RF=RandomForestClassifier()
+    model_RF.fit(new_X,y)
+    
+    feats = {}
+    for feature, importance in zip(new_X.columns, model_RF.feature_importances_):
+        feats[feature] = importance
+
+    to_drop = set()
+
+    for index in correlation.index:
+        for column in correlation.columns:
+            if index != column and correlation.loc[index, column] > 0.85:
+               ft = column if feats[column] <= feats[index] else index
+               to_drop.add(ft)
+    print("qtd de features removidas:", len(to_drop))
+
+    new_X = new_X.drop(columns = to_drop)
+    new_X['class'] = y
+    new_X.to_csv(output_file, index=False)
+    print("Dataset final criado")
 
 def parse_args(argv):
     base_parser = get_base_parser()
