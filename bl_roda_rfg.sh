68e30167c8ffb7692dcdbde489b8821aa7bde616
Diego Kreutz <diego.unipampa@gmail.com>
2022-06-21 14:38:13 -0300

    rfg requirements

--- bl_roda_rfg.sh
+++ bl_roda_rfg.sh
@@ -2,7 +2,7 @@ echo "Preparando ambiente virtual ..."
 
 VENV="venv-rfg"
 PYTHON="${VENV}/bin/python3"
-PIP="${VENV}/bin/pip3"
+PIP="pip3"
 BASE_DIR="methods/RFG"
 CANT_INSTALL_VENV_MESSAGE="ERRO: não foi possível criar o ambiente virtual (venv).\nO python3.8 e o python3.8-venv estão instalados? Se não, use o comando: sudo apt install python3.8 python3.8-venv"
 
@@ -15,7 +15,6 @@ if ! [[ -d "$VENV" ]]; then
   python3 -m venv "$VENV"
   [[ $? == 1 || ! -d "$VENV" ]] && echo -e "$CANT_INSTALL_VENV_MESSAGE">&2 && exit 1
 fi
-[[ ! -f $PIP ]] && echo "ERRO: ${PIP} não encontrado">&2 && exit 1
 
 $PIP install numpy==1.22.3 wheel
 $PIP install -r $BASE_DIR/requirements.txt
